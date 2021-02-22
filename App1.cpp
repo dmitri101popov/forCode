2f4ba66ff84bf69fe5d3c5f8e901faddc0040fcf
dmitri101popov <dmitri101.popov@gmail.com>
2021-02-22 14:04:45 +0300

    Add file

---
+++ App1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	for (int i = 0; i < str.size(); i++) {
+		if (i != str.size() - 1) { //Убраю в коцне 10^0
+			cout << str[i] << "* 10^" << str.size() - (i + 1); // тут понятно
+			if (i < str.size() - 1) {
+				cout << " + ";
+			}
+		}
+		else
+		{
+			cout << str[i];
+		}
+	}
+
+
+	return 0;
+}
